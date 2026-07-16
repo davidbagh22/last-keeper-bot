4a9be303faab342e585ff09385293f5482574076
davidbagh22 <davidbagh22@gmail.com>
2026-07-16 23:18:38 +0400

    Подключить единый главный экран администратора

--- team_quest.py
+++ team_quest.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from aiogram import Router
 
+import admin_home
 import quest_admin
 import quest_common
 import quest_control
@@ -9,8 +10,8 @@ import quest_games
 import quest_route
 
 router = Router(name='last_keeper_team_quest')
-# Центр управления подключается первым: он заменяет ввод кодов участниками
-# на подтверждение этапов Архивариусом и перехватывает экран текущей точки.
+# Единая админ-панель и центр управления подключаются первыми.
+router.include_router(admin_home.router)
 router.include_router(quest_control.router)
 router.include_router(quest_route.router)
 router.include_router(quest_games.router)
